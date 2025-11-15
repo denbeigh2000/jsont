c27443f1a6476d8144996ffab825e6720f122626
Denbeigh Stevens <denbeigh.stevens@discordapp.com>
2025-11-15 00:13:51 +0000

    permit compiling only when .so doesn't exist

--- lib/jsont/nif_bridge.ex
+++ lib/jsont/nif_bridge.ex
@@ -1,7 +1,18 @@
 defmodule Jsont.NifBridge do
+  # NOTE: this is kinda meh, but:
+  # - this keeps the package compilable when unpatch
+  # - keeping a prefix here makes it very difficult to get into a bad
+  #   compilation state, both before and after patching.
+  # - this will go away when we are back on a hex package, and we can jsut
+  #   apply the conditional compilation patch locally.
+  nif_path = "priv/_DISCORD_SPECIFIC_PREFIX_TO_BE_PATCHED_OVER_/libjsont_nif"
+  @so_exists File.exists?("#{nif_path}.so")
+
   use Rustler,
     otp_app: :jsont,
-    crate: "jsont_nif"
+    crate: "jsont_nif",
+    skip_compilation?: @so_exists,
+    load_from: {:jsont, nif_path}
 
   @spec encode(term(), boolean(), boolean()) :: {:ok, String.t()} | {:error, any()}
   def encode(_value, _bigint_as_string, _skip_elixir_struct),
